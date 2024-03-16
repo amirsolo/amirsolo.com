ab2219786c38995905265c252c448cc0f734944a
Amir Solo <me@amirsolo.com>
2024-03-16 22:41:31 +0330

    Update Container.jsx
    Added a banner on top of the website to announce the content is out dated.

--- components/Container.jsx
+++ components/Container.jsx
@@ -38,6 +38,18 @@ export default function Container({ children, ...customMeta }) {
           <meta property='article:published_time' content={meta.date} />
         )}
       </Head>
+      <div style={{
+        backgroundColor: '#f0f4f8', // subtle modern color
+        color: '#333',
+        textAlign: 'center',
+        padding: '10px 0',
+        fontSize: '16px',
+        fontWeight: 'bold',
+        width: '100%',
+        height: '60px',
+      }}>
+        Heads up! This site was crafted years ago and stands as an archive. For the latest about me, check out my <a href="https://www.linkedin.com/in/amirsolo/" style={{color: '#0077b5'}}>LinkedIn</a>.
+      </div>
       <Nav />
       <main className='flex flex-col justify-center bg-gray-50 dark:bg-dark-primary max-w-3xl mx-auto py-8 sm:py-10 px-7 sm:px-6 md:px-5'>
         {children}
